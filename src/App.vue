7e2f954f134fd0381de32b80465c571c7815167a
Liang-Shih Lin <liangshihlin@yahoo.com>
2020-09-09 14:38:35 +0800

    Moved vuejs from v3-v2 because v3 unstable and not widely supported yet

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+#app
+  .container
+    b-navbar
+      template(slot="brand")
+        b-navbar-item(tag="router-link", :to="{ name: 'home' }")
+          img(src="./assets/logo.png")
+      template(slot="start")
+        b-navbar-item(tag="router-link", :to="{ name: 'home' }") Home
+        b-navbar-item(:to="{ name: 'items' }") Items
+        b-navbar-item(:to="{ name: 'runes' }") Runes
+        b-navbar-item(:to="{ name: 'About' }") About
+    main
+      router-view
+    footer
+      p Game Version: {{ gameVersion }}
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    gameVersion: '',
+    tree: {},
+  }),
+  async created() {
+    const skillData = await import('@/assets/skilldatas/skilldata_1.00.4.json');
+    this.gameVersion = skillData.version;
+    this.tree = skillData.tree;
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="sass">
+*
+  font-family: 'Arcade Classic'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body, html
+  background-image: url('https://cdn.cloudflare.steamstatic.com/steam/apps/375480/page_bg_generated_v6b.jpg?t=1598634326')
+  background-position: center top
+  background-attachment: fixed
+  background-repeat: no-repeat
+  background-size: cover
+  color: white
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@font-face
+  font-family: 'Arcade Classic'
+  src: url('./assets/fonts/ARCADECLASSIC.TTF') format('truetype')
+  font-style: normal
+  font-weight: normal
+  text-rendering: optimizeLegibility
 </style>
---
+++ src/components/Gears.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  name: 'Gears',
+};
+</script>
+
+<style lang="sass">
+.item
+  .true-legendary
+    color: #AE5D41
+
+.skill
+  color: white
+  .name
+    color: #A98A71
+</style>
