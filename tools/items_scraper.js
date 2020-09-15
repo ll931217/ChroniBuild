97ab99c5cbc5013f2e0ae8940abfb2daf581307f
Liang-Shih Lin <liangshihlin@yahoo.com>
2020-09-16 07:22:50 +0800

    Added scrapers

---
+++ tools/items_scraper.js
@@ -0,0 +1,45 @@
+import fsModule from 'fs';
+import path from 'path';
+import util from 'util';
+
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const fs = fsModule.promises;
+const url = 'https://chronicon.fandom.com/wiki/True_Legendary_Items';
+
+(async () => {
+  const { data: html } = await axios.get(url);
+  const $ = cheerio.load(html);
+
+  const children = $('section#WikiaPage .WikiaPageContentWrapper article#WikiaMainContent #WikiaMainContentContainer #WikiaArticle #mw-content-text.mw-content-ltr.mw-content-text').children()
+  const tree = []
+
+  let index = 0
+  children.not('figure,noscript,#incontent_player.gpt-ad,p,nav').each(function() {
+    if ($(this).is('h2')) {
+      if (tree.length > index) {
+        index++
+      }
+      tree.push({ text: $(this).find('span.mw-headline').text().trim(), nodes: [] })
+    } else if ($(this).is('h3')) {
+      tree[index]['nodes'].push({ text: ($(this).find('span.mw-headline').text().trim()).replace('-', ''), nodes: [] })
+    } else {
+      $(this).find('li').each(function() {
+        if ($(this).text() !== '' || $(this).find('a').text() !== '') {
+          const text = $(this).text() !== '' ? $(this).text().trim() : $(this).find('a').text().trim()
+          const url = $(this).find('a').attr('href') ? 'https://chronicon.fandom.com' + $(this).find('a').attr('href') : ''
+
+          if (tree[index]['nodes'].length > 0 && tree[index]['nodes'][0].hasOwnProperty('nodes')) {
+            tree[index]['nodes'][tree[index]['nodes'].length - 1]['nodes'].push({ text, value: url })
+          } else {
+            tree[index]['nodes'].push({ text, value: url })
+          }
+        }
+      })
+    }
+  })
+
+  await fs.writeFile(path.normalize('./items.json'), JSON.stringify(tree, null, 2))
+  console.log(util.inspect(tree, { color: true, depth: 6 }))
+})();
